da67dd3c02eff3954bce7fa19e2bc0af5b7051d6
Isis priscila <isis.cunha13!gmail.com>
2020-10-26 16:18:50 -0300

    First commit

---
+++ beersoock/src/main/java/com/devisis/beerstock/mapper/BeerMapper.java
@@ -0,0 +1,16 @@
+package com.devisis.beerstock.mapper;
+
+import com.devisis.beerstock.dto.BeerDTO;
+import com.devisis.beerstock.entity.Beer;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface BeerMapper {
+
+    BeerMapper INSTANCE = Mappers.getMapper(BeerMapper.class);
+
+    Beer toModel(BeerDTO beerDTO);
+
+    BeerDTO toDTO(Beer beer);
+}
---
+++ beersoock/src/test/java/com/devisis/beerstock/BeersoockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devisis.beerstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeersoockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
